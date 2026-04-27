8ee6424f88962b4d6bf8bfae0b8a1742b9180f19
Christoffer D. Hystad <christhy@stud.ntnu.no>
2026-04-27 23:31:55 +0200

    This new script was added to store shared constants to avoid circular imports. More contants are extected to be moved here later...

---
+++ Formation_Flying_Energy_Analysis/src/constants.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+# File/dir paths and names
+BASE_CONFIG_PATH = 'Formation_Flying_Energy_Analysis/configs/base.yaml'
+MC_CONFIG_PATH = 'Formation_Flying_Energy_Analysis/configs/monte_carlo.yaml'
+OUTPUT_DATA_ROOT_DIR = Path('Formation_Flying_Energy_Analysis/output_data')
+SINGLE_OUTPUT_DATA_DIR_NAME = "single_runs"
+BATCH_OUTPUT_DATA_DIR_NAME = "batch_runs"
