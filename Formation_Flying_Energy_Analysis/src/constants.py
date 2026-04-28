8f7ef1caa810ffbd0a33de3a8c8bc4d50737d227
Christoffer D. Hystad <christhy@stud.ntnu.no>
2026-04-28 21:52:49 +0200

    Added constant for the path to where the Monte Carlo run overrides are stored

--- Formation_Flying_Energy_Analysis/src/constants.py
+++ Formation_Flying_Energy_Analysis/src/constants.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 # File/dir paths and names
 BASE_CONFIG_PATH = 'Formation_Flying_Energy_Analysis/configs/base.yaml'
+OVERRIDE_CONFIG_DIR = Path('Formation_Flying_Energy_Analysis/configs/run_overrides')
 MC_CONFIG_PATH = 'Formation_Flying_Energy_Analysis/configs/monte_carlo.yaml'
 OUTPUT_DATA_ROOT_DIR = Path('Formation_Flying_Energy_Analysis/output_data')
 SINGLE_OUTPUT_DATA_DIR_NAME = "single_runs"
