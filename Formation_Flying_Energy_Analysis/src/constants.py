f1fed842c2e0f408e3b311f56945a808503a3352
Christoffer D. Hystad <christhy@stud.ntnu.no>
2026-05-01 20:57:12 +0200

    Moved SPACE_WEATHER_DATA_FILE_PATH to constants.py

--- Formation_Flying_Energy_Analysis/src/constants.py
+++ Formation_Flying_Energy_Analysis/src/constants.py
@@ -7,3 +7,4 @@ MC_CONFIG_PATH = 'Formation_Flying_Energy_Analysis/configs/monte_carlo.yaml'
 OUTPUT_DATA_ROOT_DIR = Path('Formation_Flying_Energy_Analysis/output_data')
 SINGLE_OUTPUT_DATA_DIR_NAME = "single_runs"
 BATCH_OUTPUT_DATA_DIR_NAME = "batch_runs"
+SPACE_WEATHER_DATA_FILE_PATH = "shared_input_data/msis_data/Kp_ap_Ap_SN_F107_since_2010.txt"
