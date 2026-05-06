00f40cea5519d457e830c0ad8526704cb82fa88d
Christoffer D. Hystad <christhy@stud.ntnu.no>
2026-05-06 22:23:56 +0200

    Moved constants from BasiliskSImulator

--- Formation_Flying_Energy_Analysis/src/constants.py
+++ Formation_Flying_Energy_Analysis/src/constants.py
@@ -8,3 +8,17 @@ OUTPUT_DATA_ROOT_DIR = Path('Formation_Flying_Energy_Analysis/output_data')
 SINGLE_OUTPUT_DATA_DIR_NAME = "single_runs"
 BATCH_OUTPUT_DATA_DIR_NAME = "batch_runs"
 SPACE_WEATHER_DATA_FILE_PATH = "shared_input_data/msis_data/Kp_ap_Ap_SN_F107_since_2010.txt"
+VIZARD_SAVE_PATH = "/home/chris/code/formation-flight-gnssr-simulator/Formation_Flying_Energy_Analysis/output_data/_VizFiles/bsk_sim.bin"
+
+# Model rates [<time>/update] TODO: Move to Config
+ENV_RATE: float = 0.5 # [s/update] Update rate for environment models
+DYN_RATE: float = 0.5 # [s/update] Update rate for dynamical models
+FSW_RATE: float = 0.5 # [s/update] Update rate for flight software stack
+FORM_CTRL_RATE: float = 0.5 # [s/update] TODO: Update rate for the formation flight stack 
+MSIS_RATE: float = 30. # [s/update] Update rate for MSIS input parameters
+FLUSH_RATE: float = 3. # [hour/update]
+
+# Recorder sample rates [s/sample]
+HIGH_SAMPLE_RATE: float = 0.5 # [s/sample] NOTE: Must be integer multilple of 'DYN_RATE'
+MID_SAMPLE_RATE: float = 5. # [s/sample] NOTE: Must be integer multilple of 'DYN_RATE'
+LOW_SAMPLE_RATE: float = 30. # [s/sample] NOTE: Must be integer multilple of 'DYN_RATE'
